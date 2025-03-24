e8a976cbd99da7619d03ae4b34b394f15d453f6f
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-24 23:54:28 +0300

    completed 4 exercise

--- solutions/4-using.js
+++ solutions/4-using.js
@@ -1,3 +1,23 @@
 // BEGIN
+export const getDomainInfo = (filepath) => {
+    let scheme;
+    let name;
+    if (filepath.startsWith('https://')) {
+      scheme = 'https';
+    } else {
+      scheme = 'http';
+    }
+    if (filepath.startsWith('https://')) {
+      name = filepath.replace('https://', '');
+    } else if (filepath.startsWith('http://')) {
+      name = filepath.replace('http://', '');
+    } else {
+      name = filepath;
+    }
+    const info = {scheme: scheme, name: name};
+  
+    return info;
+  }
 
+  export default getDomainInfo;
 // END
