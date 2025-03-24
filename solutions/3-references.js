a684d97e35526b40990d31f869232d3b07d38f0b
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-24 23:25:23 +0300

    completed 3 exercise

--- solutions/3-references.js
+++ solutions/3-references.js
@@ -1,3 +1,11 @@
 // BEGIN
+export const getVerified = (company1, company2) => {
+    if (company1.name === company2.name && company1.state === company2.state && company1.website === company2.website) {
+      return true;
+    } else {
+      return false
+    }
+  }
 
+  export default getVerified;
 // END
