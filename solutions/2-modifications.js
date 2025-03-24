8b4ae45a113a4a30ea99905bdd373eb4501cbe01
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-24 23:15:14 +0300

    completed 2 exercise

--- solutions/2-modifications.js
+++ solutions/2-modifications.js
@@ -1,5 +1,13 @@
 import _ from 'lodash';
 
 // BEGIN
+export const getNormalization = (lesson) => {
+  let lessonName = lesson.name;
+  lesson.name = _.capitalize(lessonName);
+  let lessonDescription = lesson.description;
+  lesson.description = lessonDescription.toLowerCase();
+  return lesson;
+}
 
+export default getNormalization;
   // END
