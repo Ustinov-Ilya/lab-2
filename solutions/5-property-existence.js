1442352eceb4ffe1961197fca3dac5b4bd1d6eb1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 00:08:52 +0300

    completed 5 exercise

--- solutions/5-property-existence.js
+++ solutions/5-property-existence.js
@@ -1,5 +1,20 @@
-import _ from 'lodash';
+import _, { lowerCase } from 'lodash';
 
 // BEGIN
+export const getCountWords = (text) => {
+    const result = {};
+    const lowerText = text.toLowerCase();
+    const arr = _.words(lowerText);
 
+    for (const name of arr) {
+        if (Object.hasOwn(result, name)) {
+            result[name] += 1;
+        } else {
+            result[name] = 1;
+        }
+    }
+    return result;
+}
+
+export default getCountWords;
 // END
