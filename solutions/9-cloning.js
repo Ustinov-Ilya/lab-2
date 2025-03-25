785ee9260ce0d06369d6b8ced902388f5149126b
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 15:05:22 +0300

    completed 9 exercise

--- solutions/9-cloning.js
+++ solutions/9-cloning.js
@@ -1,3 +1,14 @@
-// BEGIN
+import { keyBy } from "lodash";
 
+// BEGIN
+export const getClone = (obj) => {
+    let result = {};
+    let entries = Object.entries(obj);
+    for (const [key, value] of entries) {
+        result[key] = value;
+    }
+    return result;
+}
+
+export default getClone;
 // END
