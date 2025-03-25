53967f30bb79ed316cef310bd697993156d2d0fa
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 17:55:36 +0300

    completed 11 exercise

--- solutions/11-destructuring.js
+++ solutions/11-destructuring.js
@@ -1,3 +1,12 @@
 // BEGIN
+export const getUserNames = (listOfUsers) => {
+    const result = [];
+    for (let users of listOfUsers) {
+        const {name} = users;
+        result.push(name);
+    }
+    return result.sort();
+}
 
+export default getUserNames;
 // END
