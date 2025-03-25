ec3b63b08a88d30b4002d1ee02d7a38d5c41d63a
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 14:48:03 +0300

    completed 8 exercise

--- solutions/8-merging.js
+++ solutions/8-merging.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
 // BEGIN
+export const getMarege = (obj, keys, dataForMerging) => {
+    let result;
+    if (keys.length > 0) {
+        let data = _.pick(dataForMerging, keys);
+        return result = _.merge(obj, data);
+    } else {
+        return result = _.merge(obj, dataForMerging);
+    }
+}
 
+export default getMarege;
 // END
