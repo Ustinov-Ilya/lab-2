f23d7e089b0621ab3c5bdea8962e18a58b9e7ca4
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 17:34:29 +0300

    completed 10 exercise

--- solutions/10-spread-operator.js
+++ solutions/10-spread-operator.js
@@ -1,3 +1,9 @@
 // BEGIN
+export const getCompanyObject = (companyName, obj) => {
+    const company = {name: companyName, state: 'moderating', createdAt: Date.now()};
+    const mergetObject = {...company, ...obj};
+    return mergetObject;
+}
 
+export default getCompanyObject;
 // END
