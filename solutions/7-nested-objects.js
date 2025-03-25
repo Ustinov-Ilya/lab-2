b3c305d3f5851a6a5211d6d45d45a1e784754d36
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-03-25 13:57:41 +0300

    completed 7 exercise

--- solutions/7-nested-objects.js
+++ solutions/7-nested-objects.js
@@ -1,3 +1,19 @@
 // BEGIN
+export const getValueKeys = (object, arrKeys) => {
+  let keys = Object.keys(object);
+  if (keys.length === 0) return null;
 
+  let result = object;
+  for (let key of arrKeys) {
+    if (Object.hasOwn(result, key)) {
+      result = result[key];
+    } else {
+      result = null;
+      break
+    }
+  }
+  return result;
+}
+
+export default getValueKeys;
 // END
